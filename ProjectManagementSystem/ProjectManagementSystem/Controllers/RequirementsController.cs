abfb0e2e4928f5aef40cdf8a09464f0b8b186013
eoneillspsu <eoneill@spsu.edu>
2014-10-09 13:54:13 -0400

    Created pass through controller for requirements views.

---
+++ ProjectManagementSystem/ProjectManagementSystem/Controllers/RequirementsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectManagementSystem.Controllers
+{
+    public class RequirementsController : Controller
+    {
+        // GET: Requirements
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
